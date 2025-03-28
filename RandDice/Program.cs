4110c0923dc9f35895fe17516248472c81a310fb
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-28 00:48:12 +0000

    Exercise 5 while loop

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -9,6 +9,12 @@ namespace RandDice
             Random random = new Random(int.Parse(args[1]));
             int n = 0;
             int maxValue = 0;
+             while (n < int.Parse(args[0]))
+            {
+                maxValue += random.Next(1, 6);
+                n++;
+            }
+            System.Console.WriteLine(maxValue);
         }
     }
 }
