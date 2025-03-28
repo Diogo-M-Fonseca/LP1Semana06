bb7aa6a3c1f49d1db7530edec47f628969cd320d
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-28 01:57:24 +0000

    Input of enemys name

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -8,6 +8,12 @@ namespace MyGame
         private static void Main(string[] args)
         {
             Enemy[] enemies = new Enemy[int.Parse(args[0])];
+
+            for (int numOfEnemies = 0; numOfEnemies < int.Parse(args[0]); numOfEnemies++)
+            {
+            Console.Write($"Nome do inimigo {numOfEnemies + 1}: ");
+            enemies[numOfEnemies] = new Enemy(Console.ReadLine());
+            }
         }
     }
 }
