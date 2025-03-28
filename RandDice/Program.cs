68efbf5d4ae1707cf1ad56279e4b384801fbba6b
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-28 00:45:45 +0000

    Exercise 5 setup

---
+++ RandDice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random random = new Random(int.Parse(args[1]));
+            int n = 0;
+            int maxValue = 0;
+        }
+    }
+}
