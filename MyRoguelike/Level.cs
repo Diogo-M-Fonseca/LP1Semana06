416ac4fcba5392b29661c65e9f903443b94db4dd
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-28 17:11:58 +0000

    new level constructor

---
+++ MyRoguelike/Level.cs
@@ -0,0 +1,17 @@
+namespace MyRoguelike
+{
+    public class Level
+    {
+       private int romnum;
+       private Toughness dificulty;
+
+        public Level(int romnum, Toughness dificulty)
+       {
+            this.romnum = romnum;
+            this.dificulty = dificulty;
+       }
+
+       
+
+    }
+}
